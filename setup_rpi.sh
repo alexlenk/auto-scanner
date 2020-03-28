bf42f3f7847edff0873edb75c8a54d278f4be90b
Alex Lenk <alexander.lenk@lenkmail.de>
2020-03-28 13:16:28 +0100

    Initial commit make Rpi RO

--- setup_rpi.sh
+++ setup_rpi.sh
@@ -16,5 +16,27 @@ if [ "first" = "true" ]; then
         apt-get update && apt-get upgrade -y && apt-get install -y docker-compose
     else
         apt-get update && apt-get upgrade -y && apt-get install -y inotify-tools s-nail psmisc poppler-utils
+
+        if [ "$1" = "ro" ]; then
+            apt-get remove --purge wolfram-engine triggerhappy anacron logrotate dphys-swapfile xserver-common lightdm
+            apt-get autoremove --purge
+            apt-get install busybox-syslogd
+            dpkg --purge rsyslog
+            echo `cat /boot/cmdline.txt` fastboot noswap ro > /boot/cmdline.txt
+            rm -rf /var/lib/dhcp /var/lib/dhcpcd5 /var/run /var/spool /var/lock /etc/resolv.conf
+            ln -s /tmp /var/lib/dhcp
+            ln -s /tmp /var/lib/dhcpcd5
+            ln -s /tmp /var/run
+            ln -s /tmp /var/spool
+            ln -s /tmp /var/lock
+            touch /tmp/dhcpcd.resolv.conf
+            ln -s /tmp/dhcpcd.resolv.conf /etc/resolv.conf
+
+            # Edit /etc/systemd/system/dhcpcd5
+
+            rm /var/lib/systemd/random-seed
+            ln -s /tmp/random-seed /var/lib/systemd/random-seed
+
+            # edit /lib/systemd/system/systemd-random-seed.service
     fi
 fi
