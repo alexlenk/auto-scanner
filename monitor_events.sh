c45cc327430532322e1628e49aef1316503f5f31
Alex Lenk <alexander.lenk@lenkmail.de>
2020-03-29 17:45:23 +0200

    RO FS and add new monitoring option

---
+++ monitor_events.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "Working dir: $DIR"
+
+log=""
+last_time=0
+merge=false
+time=$(date +%s)
+start=true
+
+echo "MONITOR: Starting ..."
+if [ -d "/volumes/SCANNER/DCIM/200DOC" ]; then
+    last_files=$(ls /volumes/SCANNER/DCIM/200DOC)
+fi
+
+if [ ! -f $DIR/.env ]; then
+    echo "Copy .env File"
+    cp /media/SCANNER/.env $DIR
+fi
+
+pipe=/tmp/scannerpipe
+
+trap "rm -f $pipe" EXIT
+
+if [[ ! -p $pipe ]]; then
+    echo "MONITOR: Creating pipe: $pipe"
+    if [[ -f $pipe ]]; then
+        rm -f $pipe
+    fi
+    mkfifo $pipe
+fi
+
+echo "MONITOR: Starting monitoring ..."
+while true; do
+    if read file action <$pipe; then
+        time=$(date +%s)
+        ((time_diff=time-last_time))
+
+        sleep 0.2
+        if [ "$action" = "CREATE,ISDIR" ]; then
+            # Started
+            if [ "$last_files" == "" ]; then
+                echo "MONITOR: Resetting Last Files"
+                i=0
+                while [[ $i -lt 40 ]] && [[ ! -d /volumes/SCANNER/DCIM/200DOC ]]
+                do
+                    sleep 0.5
+                    ((i++))
+                done
+                last_files=$(ls /volumes/SCANNER/DCIM/200DOC)
+                echo "MONITOR: Last Files Reset done"
+                echo "MONITOR: Waited for $i/2 seconds"
+            fi
+
+            if [ "$backlog_time" != "" ]; then  
+                backlog_time=""
+                backlog_action=""
+                skip=false
+
+                last_file=$pdffile
+
+                #TODO: if more than one, it should be a merging case, check merge variable
+                i=0
+                while [[ $i -lt 40 ]] && [[ ! -d /volumes/SCANNER/DCIM/200DOC ]]
+                do
+                    sleep 0.5
+                    ((i++))
+                done
+                echo "MONITOR: Waited for $i/2 seconds"
+
+                if [ -d /volumes/SCANNER/DCIM/200DOC ]; then
+                    pdffile=`diff <(echo "$last_files") <(echo "$(ls /volumes/SCANNER/DCIM/200DOC)") | grep ">" | cut -c3-`
+                    echo "MONITOR: New File in Folder: $pdffile"
+                    
+                    time_last=$time
+                    if [ "$merge" == "true" ]; then
+                        if [ "${#merge_files[@]}" -eq "0" ]; then
+                            echo "MONITOR: Creating merge list with $last_file"
+                            merge_files=( $last_file )
+                        fi
+                        echo "MONITOR: Adding file to merge list: $pdffile"
+                        merge_files+=( $pdffile )
+                    else
+                        merge_files=( $pdffile )
+                    fi
+                    
+                    $DIR/upload.sh ${merge_files[@]} &
+                    pid=$!
+
+                    echo "MONITOR: Uploading (PID $pid): ${merge_files[@]}"
+                else
+                    echo "MONITOR: Skipping upload - New Scan in Progress"
+                    skip=true
+                fi
+            fi
+
+            if [ "$skip" = "false" ]; then
+                echo "MONITOR: Updating Last Files"
+                last_files=$(ls /volumes/SCANNER/DCIM/200DOC)
+            fi
+            last_time=$(date +%s)
+        else
+            if [ "$time_diff" -le "3" -a "$start" != "true" ]; then
+                kill -9 $pid
+                merge=true
+                echo "MONITOR: Upload stopped (PID: $pid)"
+            else
+                if [ "$merge" == "true" ]; then
+                    echo "MONITOR: Ending Previous Merging Process"
+                    merge=false
+                fi
+
+                echo "MONITOR: Resetting merge list"
+                merge_files=()
+                last_file=""
+            fi
+
+            backlog_time=$time
+            backlog_action=$action
+        fi
+        
+        start=false
+    fi
+done
--- setup_rpi.sh
+++ setup_rpi.sh
@@ -4,6 +4,7 @@ if [ ! -d /home/pi/auto-scanner ]; then
     echo "First run ..."
     first=true
     ln -s /media /volumes
+    mkfifo /tmp/scannerpipe
 fi
 
 if [ "$first" = "true" ]; then
@@ -19,39 +20,45 @@ if [ -d /home/pi/auto-scanner ]; then
     cd /home/pi/auto-scanner
     git pull
     chown -R pi:pi ../auto-scanner/
-    #chmod +x *.sh
 fi
 
 cp /home/pi/auto-scanner/11-media-by-label-auto-mount.rules /etc/udev/rules.d/
 udevadm control --reload-rules
 
 if [ "$1" = "ro" ]; then
-    apt-get remove --purge wolfram-engine triggerhappy anacron logrotate dphys-swapfile xserver-common lightdm
+    apt-get remove --purge triggerhappy logrotate dphys-swapfile
     apt-get autoremove --purge
-    apt-get install busybox-syslogd
-    dpkg --purge rsyslog
     echo `cat /boot/cmdline.txt` fastboot noswap ro > /boot/cmdline.txt
-    rm -rf /var/lib/dhcp /var/lib/dhcpcd5 /var/run /var/spool /var/lock /etc/resolv.conf
-    ln -s /tmp /var/lib/dhcp
-    ln -s /tmp /var/lib/dhcpcd5
-    ln -s /tmp /var/run
-    ln -s /tmp /var/spool
-    ln -s /tmp /var/lock
-    touch /tmp/dhcpcd.resolv.conf
-    ln -s /tmp/dhcpcd.resolv.conf /etc/resolv.conf
-
-    sed -i "s/PIDFile=\/run\/dhcpcd.pid/PIDFile=\/var\/run\/dhcpcd.pid\n/g" /etc/systemd/system/dhcpcd5.service
-
-    rm /var/lib/systemd/random-seed
-    ln -s /tmp/random-seed /var/lib/systemd/random-seed
-
-    sed -i "s/\[Service\]/\[Service\]\nExecStartPre=\/bin\/echo \"\" >\/tmp\/random-seed/g" /etc/systemd/system/dhcpcd5.service
-
-    systemctl daemon-reload
+    apt-get install busybox-syslogd
+    apt-get remove --purge rsyslog
 
     sed -i "s/\/boot           vfat    defaults/\/boot           vfat    defaults,ro/g" /etc/fstab
     sed -i "s/\/               ext4    defaults,noatime/\//               ext4    defaults,noatime,ro  0       1/g" /etc/fstab
-    echo "tmpfs           /tmp            tmpfs   nosuid,nodev         0       0" >> /etc/fstab
-    echo "tmpfs           /var/log        tmpfs   nosuid,nodev         0       0" >> /etc/fstab
-    echo "tmpfs           /var/tmp        tmpfs   nosuid,nodev         0       0" >> /etc/fstab
+    echo "tmpfs        /tmp            tmpfs   nosuid,nodev         0       0" >> /etc/fstab
+    echo "tmpfs        /var/log        tmpfs   nosuid,nodev         0       0" >> /etc/fstab
+    echo "tmpfs        /var/tmp        tmpfs   nosuid,nodev         0       0" >> /etc/fstab
+
+    rm -rf /var/lib/dhcp /var/lib/dhcpcd5 /var/spool /etc/resolv.conf
+    ln -s /tmp /var/lib/dhcp
+    ln -s /tmp /var/lib/dhcpcd5
+    ln -s /tmp /var/spool
+    touch /tmp/dhcpcd.resolv.conf
+    ln -s /tmp/dhcpcd.resolv.conf /etc/resolv.conf
+
+    rm /var/lib/systemd/random-seed
+    ln -s /tmp/random-seed /var/lib/systemd/random-seed
+    sed -i "s/\[Service\]/\[Service\]\nExecStartPre=\/bin\/echo \"\" >\/tmp\/random-seed/g" /lib/systemd/system/systemd-random-seed.service
+
+    cat >/etc/bash.bashrc <<EOL
+    set_bash_prompt() {
+    fs_mode=$(mount | sed -n -e "s/^\/dev\/.* on \/ .*(\(r[w|o]\).*/\1/p")
+    PS1='\[\033[01;32m\]\u@\h${fs_mode:+($fs_mode)}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    }
+    alias ro='sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot'
+    alias rw='sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot'
+    PROMPT_COMMAND=set_bash_prompt 
+EOL
+
+    echo "mount -o remount,ro /" >> /etc/bash.bash_logout
+    echo "mount -o remount,ro /boot" >> /etc/bash.bash_logout
 fi
