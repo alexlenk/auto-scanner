cc2e173bc8a5484dd9b13db05a2e50160ef3a8ba
Alex Lenk <alexander.lenk@lenkmail.de>
2021-05-24 15:02:13 +0200

    removed sleep

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-sleep 5
-
 /auto-scanner/monitor.sh /media/SCANNER/DCIM/200DOC & > /media/STICK/auto-scanner-pdf.log
 /auto-scanner/monitor.sh /media/SCANNER/DCIM/100PHOTO & > /media/STICK/auto-scanner-jpg.log
