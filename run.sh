3ab5992dd89834e7cbb017590844bbd6eddf4f8d
Alex Lenk <alexander.lenk@lenkmail.de>
2021-05-24 15:00:01 +0200

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/auto-scanner/monitor.sh /media/SCANNER/DCIM/200DOC > /media/STICK/auto-scanner-pdf.log
-/auto-scanner/monitor.sh /media/SCANNER/DCIM/100PHOTO > /media/STICK/auto-scanner-jpg.log
+/auto-scanner/monitor.sh /media/SCANNER/DCIM/200DOC & > /media/STICK/auto-scanner-pdf.log
+/auto-scanner/monitor.sh /media/SCANNER/DCIM/100PHOTO & > /media/STICK/auto-scanner-jpg.log
