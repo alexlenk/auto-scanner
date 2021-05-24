223d09e017f048f889ffa8ce0439ce0d918d7d6c
Alex Lenk <alexander.lenk@lenkmail.de>
2021-05-24 15:08:11 +0200

    Keep run.sh running with loop

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 /auto-scanner/monitor.sh /media/SCANNER/DCIM/200DOC & > /media/STICK/auto-scanner-pdf.log
-/auto-scanner/monitor.sh /media/SCANNER/DCIM/100PHOTO > /media/STICK/auto-scanner-jpg.log
+/auto-scanner/monitor.sh /media/SCANNER/DCIM/100PHOTO & > /media/STICK/auto-scanner-jpg.log
+
+
+while true; do 
+	sleep 60
+done
