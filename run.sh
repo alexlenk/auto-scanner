e0c6cb914fc2ccc0c479dab3f1b9bfdc4f7e38ea
Alex Lenk <alexander.lenk@lenkmail.de>
2021-05-24 15:01:35 +0200

    added sleep

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sleep 5
+
 /auto-scanner/monitor.sh /media/SCANNER/DCIM/200DOC & > /media/STICK/auto-scanner-pdf.log
 /auto-scanner/monitor.sh /media/SCANNER/DCIM/100PHOTO & > /media/STICK/auto-scanner-jpg.log
