6d856e1eaf73b2124e5556ae68485bad72297583
Alex Lenk <alexander.lenk@lenkmail.de>
2021-05-24 15:14:03 +0200

    changed folder

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/auto-scanner/monitor.sh /media/SCANNER/DCIM/200DOC > /media/STICK/auto-scanner-pdf.log &
-/auto-scanner/monitor.sh /media/SCANNER/DCIM/100PHOTO > /media/STICK/auto-scanner-jpg.log &
+/auto-scanner/monitor.sh /media/SCANNER/DCIM/200DOC > /volumes/STICK/auto-scanner-pdf.log &
+/auto-scanner/monitor.sh /media/SCANNER/DCIM/100PHOTO > /volumes/STICK/auto-scanner-jpg.log &
 
 
 while true; do 
